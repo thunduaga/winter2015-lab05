3334d7b40e7895e4af40e9593abcf20023609841
thunduaga <chriswumbojumbo@msn.com>
2015-02-19 08:56:56 -0800

    i think im done the admin stuff i still need to go back and try and do page 25 and 29 from 5a
    other than those 2 pages, this lab sofar was WAY BETTER than the previous ones.
    no swear words required :)
    
    8:56am
    Signed-off-by:thunduaga <chriswumbojumbo@msn.com>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,15 +9,35 @@ class Admin extends Application {
 
     function __construct()
     {
+        
 	parent::__construct();
+        $this->load->helper('formfields');
     }
 
-    function index() {
+    function index() 
+    {
         $this->data['title'] = 'Quotations Maintenance';
         $this->data['quotes'] = $this->quotes->all();
         $this->data['pagebody'] = 'admin_list';
         $this->render();
-      } 
+    } 
+    // Add a new quotation
+    function add() 
+    {
+      $quote = $this->quotes->create();
+      $this->present($quote);
+    } 
+    // Present a quotation for adding/editing
+    function present($quote) 
+    {
+      $this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+      $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
+      $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
+      $this->data['fwhat'] = makeTextArea('The Quote', 'what', $quote->what);
+      $this->data['fsubmit'] = makeSubmitButton('Process Quote', "Click here to validate the quotation data", 'btn-success');
+      $this->data['pagebody'] = 'quote_edit';
+      $this->render();
+    } 
 
 }
 
