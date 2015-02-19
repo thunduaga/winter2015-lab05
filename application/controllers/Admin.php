b86c6c4eeef2278579304135e7202867254b3e99
thunduaga <chriswumbojumbo@msn.com>
2015-02-19 08:48:39 -0800

    added admin stuff next up is to add add() to admin.. adddddddddd
    page 12/23 5b
    
    8:48am
    Signed-off-by:thunduaga <chriswumbojumbo@msn.com>

---
+++ application/controllers/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * controllers/Admin.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class Admin extends Application {
+
+    function __construct()
+    {
+	parent::__construct();
+    }
+
+    function index() {
+        $this->data['title'] = 'Quotations Maintenance';
+        $this->data['quotes'] = $this->quotes->all();
+        $this->data['pagebody'] = 'admin_list';
+        $this->render();
+      } 
+
+}
+
+/* End of file Admin.php */
+/* Location: application/controllers/Admin.php */
